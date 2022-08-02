ec868e4054754af5f1ad390f6321c457e3edad6f
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-02 15:38:24 +0200

    Diving into property decorators

--- src/app.ts
+++ src/app.ts
@@ -33,3 +33,30 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// --
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw Error("Invalid price - should be positive");
+    }
+  }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
