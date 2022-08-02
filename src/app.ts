4bd651c3146d52f648fbda0baaf08fb0136ece5b
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-02 13:20:35 +0200

    First class decorator

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+function Logger(constructor: Function) {
+  console.log("Logging...");
+  console.log(constructor);
+}
+
+@Logger
+class Person {
+  name = "Tomek";
+
+  constructor() {
+    console.log("Creating person object...");
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
